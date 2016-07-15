8e4cdc754a707f1a2b04dab164d7fdf594a9e7c5
darren4ten <darren4ten@163.com>
2016-07-15 10:44:47 +0800

    将启动代码移到JobHost

--- JobHost/Program.cs
+++ JobHost/Program.cs
@@ -1,4 +1,5 @@
-﻿using Quartz;
+﻿using JobAPI.Model;
+using Quartz;
 using Quartz.Impl;
 using System;
 using System.Collections.Generic;
@@ -18,7 +19,24 @@ namespace JobAPI
 
         static void Test()
         {
-        
+            JobInfo jInfo = new JobInfo()
+            {
+                AssemblyName = "JobEntities",
+                FileName = @"D:\MyProjects\Test\QuartzNetProject\JobEntities\bin\Debug\JobEntities.dll",
+                StartTime = DateTime.Now.AddMinutes(-1),
+                JobName = "testJob",
+                CanConcurrent = true,
+                ClassName = "JobEntities.DemoJob",
+                CycleUnit = CycleUnitEnum.Second,
+                GroupName = "test",
+                Interval = 3,
+                RepeatMode = RepeatModeEnum.Interval
+            };
+            var list = HostScheduler.GetCurrentScheduler().GetJobGroupNames();
+            Console.WriteLine(string.Join(",", list));
+            //JobManager.AddJob(jInfo);
+            //JobManager.DeleteJob("testJob", "test");
+            HostScheduler.Start();
         }
     }
 }
