210d50b77814d13edba4308995cf3c06f30e4bbb
darren4ten <darren4ten@163.com>
2016-07-15 14:18:35 +0800

    JobHost可配置为服务，完善日志记录功能

--- JobEntities/DemoJob.cs
+++ JobEntities/DemoJob.cs
@@ -1,4 +1,5 @@
 ﻿using JobCommon;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,11 @@ namespace JobEntities
 {
     public class DemoJob : BaseJob
     {
-
+        private readonly ILog logger = LogManager.GetLogger(typeof(DemoJob));
         public override void Execute(string parameter)
         {
             Console.WriteLine("当前时间是：" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+            logger.Info(string.Format("参数{0},当前时间是{1}：", parameter, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")));
         }
     }
 }
--- JobHost/Program.cs
+++ JobHost/Program.cs
@@ -2,11 +2,14 @@
 using JobAPI.Model;
 using Quartz;
 using Quartz.Impl;
+using QuartzNetProject;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace JobAPI
 {
@@ -14,10 +17,42 @@ namespace JobAPI
     {
         static void Main(string[] args)
         {
-            Test();
+            Run();
             Console.Read();
         }
 
+        static void Run()
+        {
+            //HostFactory.Run(x =>                                 //1
+            //{
+            //    x.Service<TownCrier>(s =>                        //2
+            //    {
+            //        s.ConstructUsing(name => new TownCrier());     //配置一个完全定制的服务,对Topshelf没有依赖关系。常用的方式。
+            //        //the start and stop methods for the service
+            //        s.WhenStarted(tc => tc.Start());              //4
+            //        s.WhenStopped(tc => tc.Stop());               //5
+            //    });
+            //    x.RunAsLocalSystem();                            // 服务使用NETWORK_SERVICE内置帐户运行。身份标识，有好几种方式，如：x.RunAs("username", "password");  x.RunAsPrompt(); x.RunAsNetworkService(); 等
+
+            //    x.SetDescription("Sample Topshelf Host服务的描述");        //安装服务后，服务的描述
+            //    x.SetDisplayName("Stuff显示名称");                       //显示名称
+            //    x.SetServiceName("Stuff服务名称");                       //服务名称
+            //});    
+
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config"));
+            HostFactory.Run(x =>
+            {
+                x.UseLog4Net();
+
+                x.Service<ServiceRunner>();
+                x.RunAsLocalService();
+                x.SetDescription("QuartzDemo服务描述");
+                x.SetDisplayName("QuartzDemo服务");
+                x.SetServiceName("QuartzDemo服务名称");
+
+                x.EnablePauseAndContinue();
+            });
+        }
         static void Test()
         {
             JobInfo jInfo = new JobInfo()
